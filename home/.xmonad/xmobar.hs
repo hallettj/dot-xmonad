a51cf1a54461d42096916f4f6a159cc0ecec13be
Jesse Hallett <jesse@sitr.us>
2018-09-01 19:18:37 -0400

    Remove battery display from xmobar; reduce font size in xmobar

--- home/.xmonad/xmobar.hs
+++ home/.xmonad/xmobar.hs
@@ -7,26 +7,17 @@
 
 -- This is setup for 3200x1800 monitors
 Config {
-    font = "xft:monospace:size=10:antialias=true"
+    font = "xft:monospace:size=8:antialias=true"
     bgColor = "#002b36",
     fgColor = "#93a1a1",
     position = Static { xpos = 0, ypos = 0, width = 3000, height = 24 },
     lowerOnStart = True,
     commands = [
-        Run Battery [ "-t", "AC: <acstatus>   Batt: <left>% / <timeleft>"
-                    , "-h", "#859900"
-                    , "-n", "#859900"
-                    , "-l", "#dc322f"
-                    , "--"
-                    , "-O", "charging"
-                    , "-i", "connected"
-                    , "-o", "off"
-                    ] 60,
         Run Date "%a %b %_d 12,0%y   %H:%M" "date" 10,
         Run PipeReader "/home/jesse/.config/statnot/notification.pipe" "notifications",
         Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ <fc=#2aa198>%notifications%</fc>   %battery%   <fc=#93a1a1>%date%</fc>"
+    template = "%StdinReader% }{ <fc=#2aa198>%notifications%</fc>   <fc=#93a1a1>%date%</fc>"
 }
